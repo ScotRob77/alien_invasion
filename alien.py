5e78ea8aac54e93d3076278e35426c77dbf75208
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-09-05 15:52:00 +0000

    Add alien ASCII art in seperate file

---
+++ alien.py
@@ -0,0 +1,82 @@
+"""
+List of different stages for the alien made with
+ASCII art. File will be imported to run.py
+User will get 6 guesses before the alien is fully formed
+and the game is lost.
+Alien art taken from https://ascii.co.uk/art/aliens
+"""
+def alien_stages(remaining_attempts):
+    max_attempts = 6
+    ALIENS = ["""
+         .--.   
+        /    \ 
+        q .. p 
+         \--/  
+        __||__ """, """
+         .--.   |V|
+        /    \ _| /
+        q .. p \ /
+         \--/  //
+        __||__//
+        /.    _/ 
+        """, """
+         .--.   |V|
+        /    \ _| /
+        q .. p \ /
+         \--/  //
+        __||__//
+       /.    _/
+      // \  /
+     //   ||
+     \\  
+      )\
+     / |
+     |/\
+        """, """
+         .--.   |V|
+        /    \ _| /
+        q .. p \ /
+         \--/  //
+        __||__//
+       /.    _/
+      // \  /
+     //   ||
+     \\  /  \
+      )\|    |
+     / || || |
+     |/\
+        """, """
+         .--.   |V|
+        /    \ _| /
+        q .. p \ /
+         \--/  //
+        __||__//
+       /.    _/
+      // \  /
+     //   ||
+     \\  /  \
+      )\|    |
+     / || || |
+     |/\| || |
+        | || |
+        \ |
+      __/ |
+     \____/
+        """, """
+         .--.   |V|
+        /    \ _| /
+        q .. p \ /
+         \--/  //
+        __||__//
+       /.    _/
+      // \  /
+     //   ||
+     \\  /  \
+      )\|    |
+     / || || |
+     |/\| || |
+        | || |
+        \ || /
+      __/ || \__
+     \____/\____/
+        """]
